7b1a9df616e62b3a054a469e474c404a629a7a20
Olumuyiwa <olumuyiwa@elta.solutions>
2023-03-02 18:29:00 +0000

    pushing the change to it

--- app/Http/Controllers/LookupController.php
+++ app/Http/Controllers/LookupController.php
@@ -11,21 +11,8 @@ use Illuminate\Http\Request;
  *
  * @package App\Http\Controllers
  */
-class LookupController extends Controller
+class LookupController extends Lookup
 {
-
-    //this is private because this dependency should only be available within this class
-    private $search_repository;
-
-    /**
-     * LookupController constructor.
-     * @param SearchRepository $searchRepository
-     * construct method
-     */
-    public function __construct(SearchRepository $searchRepository)
-    {
-        $this->search_repository = $searchRepository;
-    }
 
     /**
      * @param Request $request
@@ -54,123 +41,7 @@ class LookupController extends Controller
         else{
             return $this->sendErrorResponse("Type not specified!");
         }
-
-        //We can't handle this - maybe provide feedback?
-        //die('request cannot be processed, unidentified parameters!');
     }
 
-    /**
-     * @param Request $request
-     * @return bool
-     * check if request has user id or not
-     * return true if request contains id or false otherwise
-     */
-//    public function checkIfRequestHasId(Request $request)
-//    {
-//        if(!$request->has('id')){
-//            return false;
-//        }else{
-//            return true;
-//        }
-//    }
 
-    /**
-     * @param Request $request
-     * @return array
-     * perform the minecraft lookup
-     */
-    public function lookupMinecraft(Request $request)
-    {
-        try{
-            if($request->id)
-            {
-                //lookup minecraft with user id
-                $response = $this->search_repository->lookupMinecraftById($request->id);
-            }
-            elseif ($request->username)
-            { //then request contains username otherwise
-
-                //lookup minecraft with username
-                $response = $this->search_repository->lookupMinecraftByUsername($request->username);
-
-            }else{
-                return $this->sendErrorResponse('Unrecognized parameter, Kindly include user id or username');
-            }
-
-            $arr =  $this->sendMinecraftResponse($response); //return response
-            $this->setSessionAcrossUsers($arr);
-
-            return $arr;
-
-        }catch(\Exception $e){
-            return $this->sendErrorResponse($e->getMessage());
-        }
-
-    }
-
-    /**
-     * @param Request $request
-     * @return array
-     * perform lookup on steam
-     */
-    public function lookupSteam(Request $request)
-    {
-        try{
-            if($request->id)
-            {
-                //lookup steam with id
-                $response = $this->search_repository->lookupSteamById($request->id);
-
-                $arr =  $this->sendXBLResponse($response); //return response
-                $this->setSessionAcrossUsers($arr);
-
-                return $arr;
-            }
-            elseif($request->username){
-                // request contains username
-
-                //die("Steam only supports IDs");
-                return $this->sendErrorResponse('Steam only supportd IDs.');
-            }
-            else{
-                return $this->sendErrorResponse('Unrecognized parameter, id not specified in request');
-            }
-        }catch(\Exception $e){
-            return $this->sendErrorResponse($e->getMessage());
-        }
-
-    }
-
-    /**
-     * @param Request $request
-     * @return array
-     * perform lookup on xbl
-     */
-    public function lookupXBL(Request $request)
-    {
-        try{
-            if($request->id)
-            {
-                //look up xbl with user id
-                $response = $this->search_repository->lookupXBLById($request->id);
-
-            }
-            elseif($request->username)
-            { //request contains username otherwise
-
-                //look up xbl with username
-                $response = $this->search_repository->lookupXBLByusername($request->username);
-
-            }else{
-                return $this->sendErrorResponse('Unrecognized parameter, Kindly include user id or username');
-            }
-
-            $arr =  $this->sendXBLResponse($response); //return response
-            $this->setSessionAcrossUsers($arr);
-
-            return $arr;
-        }catch(\Exception $e){
-            return $this->sendErrorResponse($e->getMessage());
-        }
-    }
 }
